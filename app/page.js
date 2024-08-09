bd50f2df55af12d04f732b00b6a11a7be700d449
mbadiao <diaombaye832@gmail.com>
2024-08-09 15:55:39 +0000

    entreprise done

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       <HeroSection />
       <Entreprise/>
       </section>
-      <CircleBleu/>
+      {/* <CircleBleu/> */}
     </>
   );
 }
--- components/CircleBleu.jsx
+++ components/CircleBleu.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import Image from "next/image";
 const CircleBleu = () => {
   return (
     <div>
-      <Image src="/circle-vector.svg" className="-z-30 -mt-64" height={1400} width={1800}  alt="circle" />
+      <Image src="/circle-vector.svg" className="-z-30 overflow-hidden -mt-64" height={1400} width={1800}  alt="circle" />
     </div>
   );
 };
